b715257574fa7be8bd0e471eb91ca59ed80ed255
Awesa <awesaatyanta.p@gmail.com>
2022-01-24 17:52:49 +0700

    Added fortune and cowsay to powershell!

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,8 @@
 $env:PSModulePath = $env:PSModulePath + "$([System.IO.Path]::PathSeparator)C:\Users\Rig\Documents\WindowsPowerShell\Modules"
+
 Import-Module posh-git
 Import-Module oh-my-posh
 Set-Theme Paradox
+
+python C:\Users\Rig\Documents\WindowsPowerShell\fortune\fortune.py | C:\Users\Rig\Documents\WindowsPowerShell\Modules\cowsay\cowsay.ps1 -f chicken
+
---
+++ fortune/fortune.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import random
+
+def read_file(filename):
+	with open(filename, 'r') as f:
+		fortunes = f.read()
+	f.closed
+	return fortunes
+
+def parse_fortunes(unparsed):
+	return unparsed.split('%')
+
+def choose_random(fortunes):
+	random_idx = random.randint(0, len(fortunes)-1)
+	return fortunes[random_idx]
+
+def main():
+	try:
+		filename = sys.argv[1]
+	except IndexError:
+		filename = os.path.join(r'C:\Users\Rig\Documents\WindowsPowerShell\fortune', 'fortunes.txt')
+
+	unparsed = read_file(filename)
+	parsed = parse_fortunes(unparsed)
+	fortune = choose_random(parsed)
+	print(fortune)
+
+if __name__ == '__main__':
+	main()
